43b828df635069d1577c6dc0ffac28abf067612d
Valdiria Vicznevski <v.valdiria@gmail.com>
2025-07-29 15:27:44 -0300

    calculadora.sh

---
+++ calculadora.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Script para executar a calculadora em Python.
+# Certifique-se de ter Python instalado.
+
+echo "Iniciando a Calculadora Python..."
+
+# Executa o script Python
+python calculadora.py
+
+echo "Calculadora finalizada. Pressione Enter para sair."
+read -n 1 -s -r -p ""
